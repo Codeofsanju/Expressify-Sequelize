a56735b2c4bf63247132e7269f5ce1dad638f0ad
Sanjeev Sharma <sanjeev.sharma90@myhunter.cuny.edu>
2019-01-20 17:03:21 -0500

    Working well with boilermaker

--- expressify.js
+++ expressify.js
@@ -1,11 +1,19 @@
-// process.argv.map((arg, index) => console.log(index, ' ' ,arg));
+#!/usr/bin/env node
 const fs = require('fs');
+// const path = require('path');
+
+
 
 const inputFile = process.argv[2];
 const outPutDir = process.argv[3];
 
 
-let inFile = require(`${inputFile}`);
+// console.log('\n',process.env.PWD, '\n');
+console.log('\n',inputFile, outPutDir, '\n');
+
+
+const inFile = require(`${inputFile}`);
+console.log(inFile);
 
 const arrFile = Object.keys(inFile).map(route => `router.use('/${route.toLowerCase()}', require('./${route.toLowerCase()}'));\n\t`).join('');
 if(Object.keys(process.argv).length !== 4){
@@ -14,50 +22,61 @@ if(Object.keys(process.argv).length !== 4){
 
 const fileMaker = (str, fileName) => {
     const path = process.cwd();
-    fs.writeFile(`${path}/db/api/${fileName}.js`, `${str}`, function(err) {
+    fs.writeFile(`${path}/server/api/${fileName}.js`, `${str}`, function(err) {
         if(err) {
             return console.log(err);
         }
-    
+
         console.log(`The ${fileName} route was saved!`);
-    }); 
+    });
 };
 
 const indexMaker = (file) => {
-    const ret = 
+    const ret =
     `//Created using Expressify for Sequelize
-    const router = require('express').Router();
-    
-\t${file}
+const router = require('express').Router();
 
-    router.use((req, res, next) => {
-\t\tconst err = new Error('API route not found!');
-\t\terr.status = 404;
-\t\tnext(err);
-    });
-    
-    module.exports = router;
-    `;
+${file}
+
+router.use((req, res, next) => {
+    const err = new Error('API route not found!');
+    err.status = 404;
+    next(err);
+});
+module.exports = router;
+`;
     return ret;
 };
 
 
 const getPostCreator = (route) => {
-    const ret = 
+    const ret =
     `//Created using Expressify for Sequalize
 const router = require('express').Router();
-const ${route} = require('../db');
+const {${route}} = require('../db/models');
 
 router.get('/', async function(req, res, next){
-\ttry {
-    const response = await ${route}.findAll({
+    try {
+        const response = await ${route}.findAll({
         include: [{all: true}]
     });
     res.send(response);
-\t} catch(error) {
-    next(error);
+    } catch(error) {
+        next(error);
     }
-});  
+});
+
+router.get('/:id', async function(req, res, next){
+  try {
+      const response = await ${route}.findOne({
+      where: {id: req.params.id},
+      include: [{all: true}]
+  });
+  res.send(response);
+  } catch(error) {
+      next(error);
+  }
+});
 
 router.post('/', async function(req, res, next){
     try {
@@ -79,50 +98,8 @@ const routesMaker = (File) => {
 
 
 
+// module.exports = () => {
+    fileMaker(indexMaker(arrFile), 'index');
+    routesMaker(inFile);
+// };
 
-console.log(fileMaker(indexMaker(arrFile), 'index'));
-routesMaker(inFile);
-
-
-
-
-
-// const outFileIndex = 
-// `//Created using Expressify for Sequelize
-// const router = require('express').Router()\n
-
-// ${inFile}
-
-// router.use((req, res, next) => {\n
-//   const err = new Error('API route not found!')\n
-//   err.status = 404\n
-//   next(err)\n
-// })\n
-
-// module.exports = router\n
-// `;
-
-// const outFileCampus =;
-// const router = require('express').Router()
-// const {Campus} = require('../db');
-
-// router.post('/', async function (req, res, next){
-//     try {
-//         const exists = await Campus.findOne({
-//             where: {id: req.body.id}
-//         })
-//         if (!exists){
-//             const newCampus = await Campus.create(req.body);
-//             res.status(201);
-//             res.send(newCampus);
-//         } else {
-//             res.sendStatus(303);
-//         }
-//     } catch (error) {
-//         next(error);
-//     }
-// });
-
-
-
-// console.log(outFileIndex);
